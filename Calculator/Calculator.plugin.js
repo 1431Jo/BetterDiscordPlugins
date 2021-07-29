0a43668cfbf52bdefa81382254d72e8e77aaa5bd
1431Jo <Lennardm26@gmail.com>
2021-07-30 00:07:10 +0200

    UPDATE YEEEET

--- Calculator/Calculator.plugin.js
+++ Calculator/Calculator.plugin.js
@@ -5,4 +5,29 @@
     * @updateUrl https://raw.githubusercontent.com/1431Jo/BetterDiscordPlugins/main/Calculator/Calculator.plugin.js
     * @website https://github.com/1431Jo/BetterDiscordPlugins/tree/main/Calculator
     * @version 0.1 (beta)
+    * 
+    * Have a nice day! ツ
 */
+
+const config = {
+    info: {
+        name: "Calculator",
+        authors: [
+            {
+                name: "Joツ"
+            }
+        ],
+        version: "0.1 (beta)",
+        description: "You can count on it in Discord.",
+    }, /*ツ*/
+    changelog: [
+        {
+            title: "Fixes",
+            type: "fixed",
+            items: [
+                ""
+            ]
+        }
+    ],
+    defaultConfig: []
+};
