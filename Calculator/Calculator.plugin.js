32c56dfde11201dcfe1bb7c06b23df2a828266f0
1431Jo <Lennardm26@gmail.com>
2021-07-29 23:45:27 +0200

    Update

---
+++ Calculator/Calculator.plugin.js
@@ -0,0 +1,8 @@
+/**
+    * @name Calculator
+    * @source https://github.com/1431Jo/BetterDiscordPlugins/blob/main/Calculator/Calculator.plugin.js
+    * @description You can count on it in Discord.
+    * @updateUrl https://raw.githubusercontent.com/1431Jo/BetterDiscordPlugins/main/Calculator/Calculator.plugin.js
+    * @website https://github.com/1431Jo/BetterDiscordPlugins/tree/main/Calculator
+    * @version 0.1 (beta)
+*/
